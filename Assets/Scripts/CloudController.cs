f7aaaff63dcbd31511f78213392f9a09252a5306
Nikita Babenko <n.babenko@carx-tech.com>
2025-05-06 18:18:03 +0300

    правки по визуалу

--- Assets/Scripts/CloudController.cs
+++ Assets/Scripts/CloudController.cs
@@ -7,15 +7,14 @@ namespace Test
 {
     public class CloudController : MonoBehaviour
     {
-        public Transform[] targets;
-
         private int m_targetIndex = 0;
         private bool m_moved = false;
         public float moveSpeed = 0.001f;
+        public Transform[] targets;
         public Cloud cloud;
         public void Action()
         {
-            Debug.Log("CloudCon - try", this);
+            Debug.Log("Cloudcontroller - try", this);
 
             if (m_moved)
             { 
@@ -32,7 +31,6 @@ namespace Test
 
             Transform target = targets[m_targetIndex];
             Vector3 targetPosition = new Vector3(target.position.x, cloud.transform.position.y, target.position.z);
-           //cloud.position = Vector3.Lerp(cloud.position, targetPosition, Time.deltaTime);
             Vector3 offset = (targetPosition - cloud.transform.position).normalized * moveSpeed * Time.deltaTime;
             if (Vector3.Distance(cloud.transform.position, targetPosition) < 0.1f)
             {
